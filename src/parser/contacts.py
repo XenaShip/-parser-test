e67a96d47992490fb16015c9b2b7927d520c977d
XenaShip <shipxena@gmail.com>
2026-01-31 20:07:35 +0300

    feat: add phone extraction

--- src/parser/contacts.py
+++ src/parser/contacts.py
@@ -1,8 +1,15 @@
 import re
-
-
+PHONE_REGEX = r"(\+?\d[\d\s\-\(\)]{8,}\d)"
 EMAIL_REGEX = r"[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+"
 
 
 def extract_emails(text):
     return set(re.findall(EMAIL_REGEX, text))
+
+def extract_phones(text):
+    raw_phones = re.findall(PHONE_REGEX, text)
+    phones = set()
+    for phone in raw_phones:
+        cleaned = re.sub(r"[^\d+]", "", phone)
+        phones.add(cleaned)
+    return phones
