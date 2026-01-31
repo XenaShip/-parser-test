2bb3ff5c71ab6ebd14a453b4e1afa93b002ce13c
XenaShip <shipxena@gmail.com>
2026-01-31 15:24:52 +0300

    create_fetch_url

--- src/parser/start_file.py
+++ src/parser/start_file.py
@@ -1,6 +1,25 @@
-def parse_site(start_url: str) -> dict:
+from .fetch_url import fetch_html
+
+def parse_site(start_url):
     return {
         "url": start_url,
         "emails": [],
         "phones": [],
     }
+
+html1 = fetch_html("https://hh.ru/vacancy/129638161?hhtmFrom=chat")
+
+if html1:
+    print("скачено")
+    print(html1[:500])
+else:
+    print("не удалось загрузить страницу")
+
+
+html2 = fetch_html("https://example.com")
+
+if html2:
+    print("скачено")
+    print(html2[:500])
+else:
+    print("не удалось загрузить страницу")
