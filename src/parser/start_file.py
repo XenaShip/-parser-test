0c28c1b8d72009d95ef97dcfbf30da510ba314c5
XenaShip <shipxena@gmail.com>
2026-02-01 20:44:41 +0300

    clean

--- src/parser/start_file.py
+++ src/parser/start_file.py
@@ -1,7 +1,5 @@
-from .fetch_html import fetch_html
-from .links import extract_links
 from .site_crawl import crawl
-from .contacts import extract_emails, extract_phones
+
 
 def parse_site(start_url):
     emails, phones = crawl(start_url, max_pages=5, delay=0)
